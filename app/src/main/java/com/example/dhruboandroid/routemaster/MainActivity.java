d00718133407ed73452619e03995022f3f2aebde
Dhrubo Android <mohibulhassan100@gmail.com>
2018-06-14 07:45:13 +0600

    Profile activity setup complete.

--- app/src/main/java/com/example/dhruboandroid/routemaster/MainActivity.java
+++ app/src/main/java/com/example/dhruboandroid/routemaster/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dhruboandroid.routemaster;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -89,6 +90,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_profile) {
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
 
         } else if (id == R.id.nav_signout) {
 
