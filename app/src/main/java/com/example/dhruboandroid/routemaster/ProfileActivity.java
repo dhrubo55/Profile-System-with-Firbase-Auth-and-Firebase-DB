0073d7e9249cf22b5da47c777565c3dbe02fb2ec
Dhrubo Android <mohibulhassan100@gmail.com>
2018-06-22 04:24:12 +0600

    Edit and save button added to profile page for editing and saving

--- app/src/main/java/com/example/dhruboandroid/routemaster/ProfileActivity.java
+++ app/src/main/java/com/example/dhruboandroid/routemaster/ProfileActivity.java
@@ -2,12 +2,28 @@ package com.example.dhruboandroid.routemaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button profile_edit;
+    private Button profile_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+         profile_edit = findViewById(R.id.profile_edit_button);
+         profile_save = findViewById(R.id.profile_save_button);
+
+         profile_edit.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 profile_save.setVisibility(View.VISIBLE);
+
+             }
+         });
     }
 }
